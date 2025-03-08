633ec72449d26d74b68fdddcc4caf886ae7fd9a9
Samuel Habib <samuel.15.fahim@gmail.com>
2025-03-08 17:37:24 -0500

    general improvements

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
+const connectDB = require('./config/db');
+connectDB();
 
 const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const taskRoutes = require('./routes/taskRoutes');  
+const userRoutes = require('./routes/userRoutes');
+
+app.use('/api/tasks', taskRoutes);
+app.use('/api/users', userRoutes);
+
+
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
 });
 
 app.listen(port, () => {
--- config/db.js
+++ config/db.js
@@ -1,9 +1,21 @@
 const mongoose = require('mongoose');
+const path = require('path');
+const fs = require('fs');
 
-require('dotenv').config();
+// Load .env from the root directory
+require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
 
 const connectDB = async () => {
     try {
+        // Check if MONGO_URI exists
+        if (!process.env.MONGO_URI) {
+            console.error('MONGO_URI is not defined in .env file');
+            console.log('Current directory:', __dirname);
+            console.log('Looking for .env file in:', path.join(__dirname, '..'));
+            console.log('Environment variables:', Object.keys(process.env).filter(key => key.includes('MONGO')));
+            throw new Error('MONGO_URI is not defined');
+        }
+        
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
